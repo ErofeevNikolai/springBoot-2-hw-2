1dc4400c9bbab6c18fb400bd0dd371786282c127
ErofeevNikolai <erofeev_n@mail.ru>
2022-11-04 16:11:48 +0500

    fixed the repository

--- src/main/java/ru/netology/springboot2hw2/repository/UserRepository.java
+++ src/main/java/ru/netology/springboot2hw2/repository/UserRepository.java
@@ -4,15 +4,14 @@ import org.springframework.stereotype.Repository;
 import ru.netology.springboot2hw2.Authorities;
 import ru.netology.springboot2hw2.User;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class UserRepository {
-    private static ConcurrentHashMap<User, Authorities> baseUsers = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<User, List<Authorities>> baseUsers = new ConcurrentHashMap<>();
     public List<Authorities> getUserAuthorities(User user) {
-        return Collections.emptyList();
+        return baseUsers.get(user);
     }
 
     public static boolean checkUser(User user){
